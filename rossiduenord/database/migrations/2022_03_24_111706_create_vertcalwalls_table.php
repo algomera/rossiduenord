e108267441a476efec24f7d834447dfee7f50886
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-03-24 16:55:50 +0100

    created project data table

--- rossiduenord/database/migrations/2022_03_24_111706_create_vertcalwalls_table.php
+++ rossiduenord/database/migrations/2022_03_24_111706_create_vertcalwalls_table.php
@@ -16,42 +16,42 @@ class CreateVertcalwallsTable extends Migration
         Schema::create('vertcalwalls', function (Blueprint $table) {
             $table->id();
             // thermical isolation intervention
-            $table->string('thermical_isolation_intervention');
+            $table->string('thermical_isolation_intervention')->nullable();
             // data
-            $table->string('total_vertical_walls');
-            $table->string('vw_realized_1');
-            $table->string('vw_realized_2');
-            $table->string('total_intervention_surface');
+            $table->string('total_vertical_walls')->nullable();
+            $table->string('vw_realized_1')->nullable();
+            $table->string('vw_realized_2')->nullable();
+            $table->string('total_intervention_surface')->nullable();
             // before realization
-            $table->string('total_expected_cost');
-            $table->string('max_possible_cost');
+            $table->string('total_expected_cost')->nullable();
+            $table->string('max_possible_cost')->nullable();
             // after realization
-            $table->string('total_isolation_cost_1');
-            $table->string('total_isolation_cost_1');
-            $table->string('final_isolation_cost');
-            $table->string('dispersing_covers');
-            $table->string('isolation_energetic_savings');
+            $table->string('total_isolation_cost_1')->nullable();
+            $table->string('total_isolation_cost_1')->nullable();
+            $table->string('final_isolation_cost')->nullable();
+            $table->string('dispersing_covers')->nullable();
+            $table->string('isolation_energetic_savings')->nullable();
             // air conditioning system replacing intervention
-            $table->string('winter_acs_replacing');
-            $table->string('total_power');
-            $table->string('generators');
+            $table->string('winter_acs_replacing')->nullable();
+            $table->string('total_power')->nullable();
+            $table->string('generators')->nullable();
             // equipment
-            $table->string('condensing_boiler');
-            $table->string('heat_pumps');
-            $table->string('absorption_heat_pumps');
-            $table->string('hybrid_system');
-            $table->string('microgeneration_system');
-            $table->string('water_heatpumps_installation');
-            $table->string('biome_generators');
-            $table->string('solar_panel');
-            $table->string('solar_panel_use');
+            $table->string('condensing_boiler')->nullable();
+            $table->string('heat_pumps')->nullable();
+            $table->string('absorption_heat_pumps')->nullable();
+            $table->string('hybrid_system')->nullable();
+            $table->string('microgeneration_system')->nullable();
+            $table->string('water_heatpumps_installation')->nullable();
+            $table->string('biome_generators')->nullable();
+            $table->string('solar_panel')->nullable();
+            $table->string('solar_panel_use')->nullable();
             // project cost
-            $table->string('total_acs_project_cost');
+            $table->string('total_acs_project_cost')->nullable();
             // effective cost
-            $table->string('total_replacing_cost_1');
-            $table->string('total_replacing_cost_1');
-            $table->string('final_replacing_cost');
-            $table->string('replacing_energetic_savings');
+            $table->string('total_replacing_cost_1')->nullable();
+            $table->string('total_replacing_cost_1')->nullable();
+            $table->string('final_replacing_cost')->nullable();
+            $table->string('replacing_energetic_savings')->nullable();
             // date
             $table->timestamps();
         });
