1be980528175cfedf7e970f33c1eaf3fc5ab22d1
fabio-alkimia <fabio@alkimiadigital.it>
2022-03-29 15:20:33 +0200

    - Fix bug errore jQuery per la ricerca;

--- rossiduenord/resources/views/business/layouts/partials/search.blade.php
+++ rossiduenord/resources/views/business/layouts/partials/search.blade.php
@@ -1,22 +1,23 @@
-<script type="text/javascript" language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"> </script>
-<script>
-      $(document).ready(function(){
-      $("#search").on("keyup", function() {
-          var value = $(this).val().toLowerCase();
-          $("#table_ContentList tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-          });
-      });
-      });
+@push('scripts')
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#search").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#table_ContentList tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
 
-      $(document).ready(function() {
+        $(document).ready(function() {
 
-       $('.checkall').click(function() {
-           $(":checkbox").attr("checked", true);
-       });
+            $('.checkall').click(function() {
+                $(":checkbox").attr("checked", true);
+            });
 
-       $('.uncheckall').click(function() {
-           $(":checkbox").attr("checked", false);
-       });
-   });
-  </script>
+            $('.uncheckall').click(function() {
+                $(":checkbox").attr("checked", false);
+            });
+        });
+    </script>
+@endpush
