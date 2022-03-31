7e135f61c477377c4ef81fa78c5a0b1e050e7bf4
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-03-31 17:31:58 +0200

    project data validation + errors + old value

--- rossiduenord/resources/views/business/superbonus/data_project.blade.php
+++ rossiduenord/resources/views/business/superbonus/data_project.blade.php
@@ -16,116 +16,174 @@
                 <div class="dati_progetto px-20 pb-20">
                     <div class="d-flex">
                         <p>È stata depositata la relazione tecnica prevista dall’art. 28 della legge 10/91 e dall’art. 8 comma 1 del D.lgs 192/05 e successive modificazioni</p>
-                        <label for="technical_report" style="margin-left: 20px">
-                            <input {{ $data_project->technical_report == 'notDefine' ? 'checked' : ''}} type="radio" value="notDefine" name="technical_report" id="technical_report">
+                        <label for="technical_report" style="margin-left: 20px"  class="px-2 form-check">
+                            <input {{ $data_project->technical_report == 'notDefine' ? 'checked' : ''}} {{old('technical_report') == 'notDefine' ? 'checked' : ''}} type="radio" value="notDefine" class="form-check-input @error('technical_report') is-invalid @enderror" name="technical_report" id="technical_report">
                             N.D.
                         </label>
-                        <label for="technical_report" style="margin-left: 20px">
-                            <input {{ $data_project->technical_report == 'no' ? 'checked' : ''}} type="radio" value="no" name="technical_report" id="technical_report">
+                        <label for="technical_report" style="margin-left: 20px" class="px-2">
+                            <input {{ $data_project->technical_report == 'no' ? 'checked' : ''}} {{old('technical_report') == 'no' ? 'checked' : ''}} type="radio" value="no" class="form-check-input @error('technical_report') is-invalid @enderror" name="technical_report" id="technical_report">
                             No
                         </label>
-                        <label for="technical_report" style="margin-left: 20px">
-                            <input {{ $data_project->technical_report == 'yes' ? 'checked' : ''}} type="radio" value="yes" name="technical_report" id="technical_report">
+                        <label for="technical_report" style="margin-left: 20px" class="px-2">
+                            <input {{ $data_project->technical_report == 'yes' ? 'checked' : ''}} {{old('technical_report') == 'yes' ? 'checked' : ''}} type="radio" value="yes" class="form-check-input @error('technical_report') is-invalid @enderror" name="technical_report" id="technical_report">
                             Si
                         </label>
                     </div>
-                    <div class="d-flex align-items-center">
-                        <p style="margin: 0">è stata depositata nell’ufficio competente del Comune di</p>
-                        <input type="text" value="{{$data_project->filed_common}}" name="filed_common" id="filed_common" style="margin-left: 20px;" class="text-center border @error('filed_common') is-invalid @enderror">
-                        @error('filed_common')
-                                <span class="invalid-feedback" role="alert">
+                    <div class="d-flex align-items-center"> 
+                        <p class="pt-4" style="margin: 0">è stata depositata nell’ufficio competente del Comune di</p> 
+                        <div class="pt-4">
+                            <input type="text" value="{{ old('filed_common') ?? $data_project->filed_common}}" name="filed_common" id="filed_common" style="margin-left: 20px; width: 150px;" class="text-center border form-control  @error('filed_common') is-invalid @enderror">
+                            @error('filed_common')
+                                <span class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                        @enderror
+                            @enderror
+                        </div> 
+                          
                         <label for="filed_province" style="margin: 0; padding-left: 20px;">
                             Prov.
-                            <input type="text" value="{{$data_project->filed_province}}" name="filed_province" id="filed_province" class="text-center border">
+                            <input type="text" value="{{old('filed_province') ?? $data_project->filed_province}}" name="filed_province" id="filed_province" class="text-center border form-control  @error('filed_province') is-invalid @enderror">
                             @error('filed_province')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </label>
                         <label for="filed_date" style="margin: 0; padding-left: 20px;">
                             in data
-                            <input type="date" value="{{$data_project->filed_date}}" name="filed_date" id="filed_date" class="text-center border">
+                            <input type="date" value="{{old('filed_date') ?? $data_project->filed_date}}" name="filed_date" id="filed_date" class="text-center border form-control @error('filed_date') is-invalid @enderror">
                             @error('filed_date')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </label>
                         <label for="filed_protocol" style="margin: 0; padding-left: 20px;">
                             Protocollo n.
-                            <input type="text" value="{{$data_project->filed_protocol}}" name="filed_protocol" id="filed_protocol" class="text-center border">
+                            <input type="text" value="{{old('filed_protocol') ?? $data_project->filed_protocol}}" name="filed_protocol" id="filed_protocol" class="text-center border form-control @error('filed_protocol') is-invalid @enderror">
                             @error('filed_protocol')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </label>
                     </div>
-                    <div class="d-flex align-items-center mt-5">
-                        <input {{ $data_project->technical_report_exclusion == 'true' ? 'checked' : ''}} type="checkbox" class="@error('technical_report_exclusion') @enderror" value="true" name="technical_report_exclusion" id="technical_report_exclusion">
-                        <p class="m-0 ml-2">Non è stata depositata la relazione tecnica in quanto si ricade nei casi di esclusione previsti dal comma 1 dell’art. 8 del del D.lgs 192/05 e dal punto 2,
-                            paragrafo 2.2. dell’allegato 1 del decreto 26/06/2015</p>
+                    <div class="d-flex align-items-center mt-5">  
+                        <div class="form-group m-0" style="display:inline-block;" >
+                            <label for="technical_report_exclusion" class="checkbox-wrapper d-flex mr-3">
+                                <input {{ $data_project->technical_report_exclusion == 'true' ? 'checked' : ''}} {{old('technical_report_exclusion') == 'true' ? 'checked' : ''}} type="checkbox" class="@error('technical_report_exclusion') is-invalid @enderror" value="true" name="technical_report_exclusion" id="technical_report_exclusion">
+                                <span class="checkmark"></span>
+                                <p class="m-0">Non è stata depositata la relazione tecnica in quanto si ricade nei casi di esclusione previsti dal comma 1 dell’art. 8 del del D.lgs 192/05 e dal punto 2,
+                                    paragrafo 2.2. dell’allegato 1 del decreto 26/06/2015
+                                </p>
+                                @error('intervention_tipology')
+                                    <span class="invalid-feedback pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </label>
+                        </div>
                     </div>
 
-                    <div class="mt-5">
-                        <label for="work_start">
+                    <div class="mt-5 d-flex align-items-center">
+                        <div>
                             - gli stessi lavori sono iniziati in data
-                            <input type="date" value="{{$data_project->work_start}}" name="work_start" id="work_start" class="text-center border @error('work_start') @enderror">
+                       </div> 
+                        <label for="work_start">
+                            <input type="date" value="{{old('work_start') ?? $data_project->work_start}}" name="work_start" id="work_start" class="text-center border form-control ml-3 @error('work_start') is-invalid @enderror">
                             @error('work_start')
-                                <span class="invalid-feedback" role="alert">
+                                <div class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
-                                </span>
+                                </div>
                             @enderror
                         </label>
-                        <label for="" style="padding-left: 20px;">
-                            e conclusi in data
-                            <input type="date" value="{{$data_project->end_of_works}}" name="end_of_works" id="end_of_works" class="text-center border @error('end_of_works') @enderror">
+                        <div class="ml-5">e conclusi in data</div>
+                        <label for="end_of_works">
+                            <input type="date" value="{{old('end_of_works') ?? $data_project->end_of_works}}" name="end_of_works" id="end_of_works" class="text-center border form-control ml-3 @error('end_of_works') is-invalid @enderror">
                             @error('end_of_works')
-                                <span class="invalid-feedback" role="alert">
+                                <div class="invalid-feedback pl-3" role="alert">
                                     <strong>{{ $message }}</strong>
-                                </span>
+                                </div>
                             @enderror
                         </label>
                     </div>
 
                     <div>
                         <p class="m-0 mt-2">- i lavori sono eseguiti su:</p>
-                        <div>
-                            <label for="type_building">
-                                <input {{ $data_project->type_building == 'condominium' ? 'checked' : ''}} type="radio" value="condominium" name="type_building" id="type_building" class="mr-2 @error('type_building') is-invalid @enderror">
-                                Edificio condominiale composto da n. unità
+                        <div class="d-flex align-items-center">
+                            <label for="type_building" class="">
+                                <input {{ $data_project->type_building == 'condominium' ? 'checked' : ''}} {{old('type_building') == 'condominium' ? 'checked' : ''}} type="radio" value="condominium" name="type_building" id="type_building" class="mr-2 @error('type_building') is-invalid @enderror">
+                                <span>Edificio condominiale composto da n. unità</span>
+                                @error('type_building')
+                                    <span class="invalid-feedback pl-3 pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </label>
-                            <input type="number" value="{{$data_project->building_unit}}" name="building_unit" id="building_unit" class="mr-4 text-center border @error('building_unit') is-invalid @enderror">
-                            <span>n.pertinenza</span>
-                            <input type="number" value="{{$data_project->relevance}}" name="relevance" id="relevance" class="mr-4 text-center border @error('relevance') is-invalid @enderror">
-                            <label for="centralized_system">
-                                <input {{ $data_project->centralized_system == 'true' ? 'checked' : ''}} type="checkbox" value="true" name="centralized_system" id="centralized_system" class="@error('centralized_system') is-invalid@enderror">
+
+                            <label for="building_unit" class="w-10 mr-5">
+                                <input type="number" value="{{old('building_unit') ?? $data_project->building_unit}}" name="building_unit" id="building_unit" class="mx-3 text-center border form-control @error('building_unit') is-invalid @enderror">
+                                @error('building_unit')
+                                    <span class="invalid-feedback pl-3 pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </label>
+
+                            <div class="h-100">
+                                n.pertinenze
+                            </div>
+
+                            <label for="relevance" class="">
+                                <input type="number" value="{{old('relevance') ?? $data_project->relevance}}" name="relevance" id="relevance" class="mx-3 text-center border form-control @error('relevance') is-invalid @enderror">
+                                @error('relevance')
+                                    <span class="invalid-feedback pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </label>
+                            <label for="centralized_system" class="checkbox-wrapper d-flex mx-5">
+                                <input {{ $data_project->centralized_system == 'true' ? 'checked' : ''}} {{old('centralized_system') == 'true' ? 'checked' : ''}} type="checkbox" value="true" name="centralized_system" id="centralized_system" class="@error('centralized_system') is-invalid @enderror">
+                                <span class="checkmark"></span>
                                 con impianto termico centralizzato
                             </label>
                         </div>
-                        <label for="type_building" class="d-flex align-items-center">
-                            <input {{ $data_project->type_building == 'single_property' ? 'checked' : ''}} type="radio" value="single_property" name="type_building" id="type_building" class="mr-2 @error('type_building') is-invalid @enderror">
-                            Unità immobiliare unifamiliare
-                        </label>
-                        <label for="type_building" class="d-flex align-items-center">
-                            <input {{ $data_project->type_building == 'multi_property' ? 'checked' : ''}} type="radio" value="multi_property" name="type_building" id="type_building" class="mr-2 @error('type_building') is-invalid @enderror">
-                            Unità immobiliari situate in edifici plurifamiliari
-                        </label>
+                        <div>
+                            <label for="type_building">
+                                <input {{ $data_project->type_building == 'single_property' ? 'checked' : ''}} {{old('type_building') == 'single_property' ? 'checked' : ''}} type="radio" value="single_property" name="type_building" id="type_building" class="mr-2  @error('type_building') is-invalid @enderror">
+                                <span>Unità immobiliare unifamiliare</span>
+                                @error('type_building')
+                                    <span class="invalid-feedback pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </label>
+                        </div>
+                        <div>
+                            <label for="type_building">
+                                <input {{ $data_project->type_building == 'multi_property' ? 'checked' : ''}} {{old('type_building') == 'multi_property' ? 'checked' : ''}} type="radio" value="multi_property" name="type_building" id="type_building" class="mr-2  @error('type_building') is-invalid @enderror">
+                                <span>Unità immobiliari situate in edifici plurifamiliari</span>
+                                @error('type_building')
+                                    <span class="invalid-feedback pl-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </label>
+                        </div>
                     </div>
-
                     <div class="d-flex align-items-center mt-4">
                         <p class="m-0">- la superficie lorda ante lavori complessiva disperdente è pari a</p>
-                        <input type="number" value="{{$data_project->gross_surface_area}}" name="gross_surface_area" id="gross_surface_area" class="text-center ml-2 mr-2 border @error('gross_surface_area') is-invalid @enderror">
-                        @error('gross_surface_area')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                        @enderror
-                        <span>m²</span>
+                        <div class="px-3">
+                            <label for="gross_surface_area">
+                                <input type="number" value="{{old('gross_surface_area') ?? $data_project->gross_surface_area}}" name="gross_surface_area" id="gross_surface_area" class="text-center border form-control @error('gross_surface_area') is-invalid @enderror">
+                                <span>m²</span>
+                                @error('gross_surface_area')
+                                        <span class="invalid-feedback pl-3" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </label>
+                        </div>
                     </div>
 
                     <div class="mt-5">
