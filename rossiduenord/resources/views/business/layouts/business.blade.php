82a93a457d9069d5e1b4f05303dc424068047eff
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-04-05 17:44:23 +0200

    fix logo and practice edit input

--- rossiduenord/resources/views/business/layouts/business.blade.php
+++ rossiduenord/resources/views/business/layouts/business.blade.php
@@ -30,7 +30,7 @@
         <div class="container-custom d-flex">
             <nav class="navbar">
                 <div class="my-4 ml-4">
-                    <img src="{{asset('img/logo.svg')}}" alt="">
+                    <img src="{{asset('img/logo.svg')}}" alt="" class="img-fluid">
                 </div>
                 <ul class="nav flex-column">
                     <li class="nav-item nav-pills">
