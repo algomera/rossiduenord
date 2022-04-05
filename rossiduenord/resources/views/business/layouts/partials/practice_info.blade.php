7afa0426422fee21a206af20b46e1764e282a7f4
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-04-05 10:51:06 +0200

    Finished bottom  fixed div

--- rossiduenord/resources/views/business/layouts/partials/practice_info.blade.php
+++ rossiduenord/resources/views/business/layouts/partials/practice_info.blade.php
@@ -2,17 +2,17 @@
 <div class="practices-info fixed-bottom py-3">
     <div class="d-flex justify-content-between align-items-center h-100">
         <div class="practice-count text-center">
-            <span class="mx-3">N° Pratiche in lista</span>
-            <input type="number" name="" id="" value="88">
+            <span class="mx-3 blue">N° Pratiche in lista</span>
+            <input type="text" class="text-center border-blue" name="" id="" value="{{count($practices)}}" >
         </div>
         <div class="practice-imports d-flex justify-content-between text-center">
             <div class="fianl-Sal mx-3">
-                <span class="mx-2">Importo SAL €</span>
-                <input type="number" name="" id="" value="11.094.755">
+                <span class="mx-2 blue">Importo SAL €</span>
+                <input type="text" class="text-center border-blue" name="" id="" value="{{number_format($tot_sal, 2, ',', '.')}}">
             </div>
             <div class="expected-Sal">
-                <span class="mx-2">Importo SAL stimato €</span>
-                <input type="number" name="" id="" value="0">
+                <span class="mx-2 blue">Importo SAL stimato €</span>
+                <input type="text" class="text-center border-blue" name="" id="" value="{{number_format($expected_sal, 2, ',', '.')}}">
             </div>
         </div>
     </div>
