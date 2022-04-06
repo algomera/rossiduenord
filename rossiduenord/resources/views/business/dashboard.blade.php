df19638c9f936e013d7262ea41cdc1f8b8db1196
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-04-06 12:45:34 +0200

    new dashboard impresa

--- rossiduenord/resources/views/business/dashboard.blade.php
+++ rossiduenord/resources/views/business/dashboard.blade.php
@@ -1,23 +1,36 @@
 @extends('business.layouts.business')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Sei un utente Impresa!') }}
-                </div>
+{{-- header --}}
+    <div class="mb-2" style="padding: 10px 165px 10px 30px; border-bottom: 2px solid rgb(219, 220, 219);">
+        <h2 class="light-grey">Dashboard</h2> 
+    </div>
+{{-- main --}}
+    <div class="d-flex px-4 my-2">
+        <div class="dash-box d-flex flex-column align-items-center justify-content-center mx-2">
+            <div class="mb-2">
+                <img src="{{asset('img/icon/ICONA LISTA PRATICHE.svg')}}" alt="" class="img-fluid">
             </div>
+            <p><b>Lista Pratiche</b></p>
+        </div>
+        <div class="dash-box d-flex flex-column align-items-center justify-content-center mx-2">
+            <div class="mb-2" >
+                <img src="{{asset('img/icon/ICONA NUOVA PRATICA.svg')}}" alt="" class="img-fluid">
+            </div>
+            <p><b>Nuova Pratica</b></p>
+        </div>
+        <div class="dash-box d-flex flex-column align-items-center justify-content-center mx-2">
+            <div class="mb-2">
+                <img src="{{asset('img/icon/ICONA GESTIONE ACCESSI.svg')}}" alt="" class="img-fluid">
+            </div>
+            <p><b>Gestione Accessi</b></p>
+        </div>
+        <div class="dash-box d-flex flex-column align-items-center justify-content-center mx-2">
+            <div class="mb-2">
+                <img src="{{asset('img/icon/ICONA GESTIONE FILE.svg')}}" alt="" class="img-fluid">
+            </div>
+            <p><b>Gestione File</b></p>
         </div>
     </div>
-</div>
+     
 @endsection
