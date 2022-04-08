213290db98eb5e7439feea429dc7a6b14eab7363
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-04-08 12:53:42 +0200

    created foto & video migration

---
+++ rossiduenord/database/migrations/2022_04_08_104402_create_video_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('practice_id')->unsigned();
+            $table->foreign('practice_id')->references('id')->on('practices')->onDelete('cascade');
+            $table->string('name')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('reference')->nullable();
+            $table->string('inspection_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+}
