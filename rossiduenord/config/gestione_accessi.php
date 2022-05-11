cba929e27fad7061eec0d0bd7771d5de1e2750aa
fabio-alkimia <fabio@alkimiadigital.it>
2022-05-11 12:12:22 +0200

    - L'utente loggato (se ha il permesso di farlo) può creare SOLO utenti determinati ruoli;

---
+++ rossiduenord/config/gestione_accessi.php
@@ -0,0 +1,38 @@
+<?php
+	return [
+		'admin' => [
+			'admin' => 'admin',
+			'financial' => 'financial',
+			'bank' => 'bank',
+			'business' => 'business',
+			'collaborator' => 'collaborator',
+			'consultant' => 'consultant',
+			'technical_asseverator' => 'technical_asseverator',
+			'fiscal_asseverator' => 'fiscal_asseverator'
+		],
+		'financial' => [
+			'bank' => 'bank',
+			'business' => 'business',
+		],
+		'bank' => [
+			'business' => 'business',
+		],
+		'business' => [
+			'collaborator' => 'collaborator',
+		],
+		'collaborator' => [
+
+		],
+		'consultant' => [
+
+		],
+		'technical_asseverator' => [
+
+		],
+		'fiscal_asseverator' => [
+
+		],
+		'manager' => [
+
+		],
+	];
