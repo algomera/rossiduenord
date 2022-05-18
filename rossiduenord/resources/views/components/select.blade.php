e1173c882d45d797a6baa805c43ac29c9cefdfd7
fabio-alkimia <fabio@alkimiadigital.it>
2022-05-18 17:54:34 +0200

    - Aggiunto componente select;

---
+++ rossiduenord/resources/views/components/select.blade.php
@@ -0,0 +1,40 @@
+@props(['disabled' => false, 'required' => false, 'name', 'label' => false])
+@php
+	$n = $attributes->wire('model')->value() ?: $name;
+	$slug = $attributes->wire('model')->value() ?: $n;
+@endphp
+@error($slug)
+@php
+	$inputClass = 'block w-full py-2 pl-3 pr-10 mt-1 text-base border-red-300 text-red-900 placeholder-red-300 rounded-md focus:outline-none disabled:bg-gray-100 disabled:text-gray-40 focus:ring-red-500 focus:border-red-500 sm:text-sm';
+@endphp
+@else
+	@php
+		$inputClass = 'block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-300 rounded-md focus:outline-none disabled:bg-gray-100 disabled:text-gray-40 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
+	@endphp
+	@enderror
+	<div>
+		<div class="flex items-center justify-between">
+			@if ($label)
+				<x-label :for="$name" :required="$required">{{ $label }}</x-label>
+			@endif
+			@isset($action)
+				{{ $action }}
+			@endisset
+			{{--		<span--}}
+			{{--				wire:click="$emit('openModal', 'teams.create-team-location')"--}}
+			{{--				class="text-sm text-indigo-500 cursor-pointer hover:text-indigo-400"--}}
+			{{--		>Crea</span>--}}
+		</div>
+		<select
+				{{ $attributes->merge(['class' => $inputClass]) }}
+				{{ $disabled ? 'disabled' : '' }}
+				name="{{ $slug }}"
+				id="{{ $slug }}"
+				{{ $required ? 'required' : '' }}
+		>
+			{{ $slot }}
+		</select>
+		@error($slug)
+		<p class="mt-1 text-xs text-red-600">{{ $message }}</p>
+		@enderror
+	</div>
