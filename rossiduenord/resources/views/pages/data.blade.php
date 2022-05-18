ad498980ab057cae002433cb57f3fc4b5fae5f1d
fabio-alkimia <fabio@alkimiadigital.it>
2022-05-18 10:00:15 +0200

    - Modifica file data.blade.php per nuovo layout;

--- rossiduenord/resources/views/pages/data.blade.php
+++ rossiduenord/resources/views/pages/data.blade.php
@@ -1,11 +1,7 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app-layout>
     <div class="content-main">
-        <div class="box px-20 pt-20 pb-20">
-            <span class="black text-md"><b>Completa il tuo Profilo</b></span>
-            <hr class="bg-black">
-
+        <x-common.page-header>Completa il tuo Profilo</x-common.page-header>
+        <div class="">
             @include('layouts.partials.error')
             <form action="{{ route('update.data', auth()->id()) }}" method="POST">
                 @csrf
@@ -17,7 +13,7 @@
                         <input id="type" type="text" style="height: 47px!important" class="form-control @error('type') is-invalid @enderror" name="type" value="{{ old('type') ?? auth()->user()->user_data->type }}" required autocomplete="type" autofocus>
 
                         @error('type')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -30,7 +26,7 @@
                         <input id="p_iva" type="text" style="height: 47px!important" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') ?? auth()->user()->user_data->p_iva }}" required autocomplete="p_iva" autofocus>
 
                         @error('p_iva')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -43,7 +39,7 @@
                         <input id="c_f" type="text" style="height: 47px!important" class="form-control @error('c_f') is-invalid @enderror" name="c_f" value="{{ old('c_f') ?? auth()->user()->user_data->c_f }}" required autocomplete="c_f" autofocus>
 
                         @error('c_f')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -56,7 +52,7 @@
                         <input id="legal_form" type="text" style="height: 47px!important" class="form-control @error('legal_form') is-invalid @enderror" name="legal_form" value="{{ old('legal_form') ?? auth()->user()->user_data->legal_form }}" required autocomplete="legal_form" autofocus>
 
                         @error('legal_form')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -69,7 +65,7 @@
                         <input id="rea" type="text" style="height: 47px!important" class="form-control @error('rea') is-invalid @enderror" name="rea" value="{{ old('rea') ?? auth()->user()->user_data->rea }}" required autocomplete="rea" autofocus>
 
                         @error('rea')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -82,7 +78,7 @@
                         <input id="c_ateco" type="text" style="height: 47px!important" class="form-control @error('c_ateco') is-invalid @enderror" name="c_ateco" value="{{ old('c_ateco') ?? auth()->user()->user_data->c_ateco }}" required autocomplete="c_ateco" autofocus>
 
                         @error('c_ateco')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -95,7 +91,7 @@
                         <input id="reg_date" type="text" style="height: 47px!important" class="form-control @error('reg_date') is-invalid @enderror" name="reg_date" value="{{ old('reg_date') ?? auth()->user()->user_data->reg_date }}" required autocomplete="reg_date" autofocus>
 
                         @error('reg_date')
-                            <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -108,4 +104,4 @@
             </form>
         </div>
     </div>
-@endsection
+</x-app-layout>
