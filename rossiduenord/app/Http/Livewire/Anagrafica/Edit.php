afccb1699b6a41da26636199b3f8f08f10fbd8ae
fabio-alkimia <fabio@alkimiadigital.it>
2022-05-23 12:42:50 +0200

    - Refactor codice Edit (anagrafica) utilizzando direttamente il model data binding;

--- rossiduenord/app/Http/Livewire/Anagrafica/Edit.php
+++ rossiduenord/app/Http/Livewire/Anagrafica/Edit.php
@@ -8,57 +8,34 @@
 
 	class Edit extends ModalComponent
 	{
-		public $anagrafica;
-		public $subject_roles;
-		public $subject_type;
-		public $consultant_type;
-		public $company_name;
-		public $first_name;
-		public $last_name;
-		public $address;
-		public $zip;
-		public $city;
-		public $province;
-		public $iban;
-		public $vat;
-		public $fiscal_code;
-		public $phone;
-		public $fax;
-		public $email;
-		public $email_pec;
-		public $ticket_code;
-		public $date_of_birth;
-		public $common_of_birth;
-		public $province_of_birth;
-		public $order_or_college;
-		public $order_or_college_province;
-		public $order_or_college_number;
+		public Anagrafica $anagrafica;
 		public $roles = [];
+
 		protected function rules() {
 			return [
-				'subject_type'              => 'required',
-				'consultant_type'           => 'nullable',
-				'company_name'              => 'required|string',
-				'first_name'                => 'required|string',
-				'last_name'                 => 'required|string',
-				'address'                   => 'nullable|string',
-				'zip'                       => 'nullable|string|min:5|regex: /[0-9]/',
-				'city'                      => 'required|string',
-				'province'                  => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
-				'iban'                      => 'nullable | string |min:32 |max:32',
-				'vat'                       => 'nullable|string',
-				'fiscal_code'               => 'nullable | string |min:16|max:16 |regex:/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i',
-				'phone'                     => 'nullable|string|min:10|regex: /[0-9]/',
-				'fax'                       => 'nullable|string|min:10|regex: /[0-9]/',
-				'email'                     => 'nullable|string|email|max:100|unique:anagrafiche,email,' . $this->anagrafica->id,
-				'email_pec'                 => 'nullable|string|email|max:100|unique:anagrafiche,email_pec,' . $this->anagrafica->id,
-				'ticket_code'               => 'nullable|string',
-				'date_of_birth'             => 'nullable|date|date_format:Y-m-d|before:today',
-				'common_of_birth'           => 'nullable|string',
-				'province_of_birth'         => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
-				'order_or_college'          => 'nullable|string',
-				'order_or_college_province' => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
-				'order_or_college_number'   => 'nullable|string|regex: /[0-9]/',
+				'anagrafica.subject_type'              => 'required',
+				'anagrafica.consultant_type'           => 'nullable',
+				'anagrafica.company_name'              => 'required|string',
+				'anagrafica.first_name'                => 'required|string',
+				'anagrafica.last_name'                 => 'required|string',
+				'anagrafica.address'                   => 'nullable|string',
+				'anagrafica.zip'                       => 'nullable|string|min:5|regex: /[0-9]/',
+				'anagrafica.city'                      => 'required|string',
+				'anagrafica.province'                  => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
+				'anagrafica.iban'                      => 'nullable | string |min:32 |max:32',
+				'anagrafica.vat'                       => 'nullable|string',
+				'anagrafica.fiscal_code'               => 'nullable | string |min:16|max:16 |regex:/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i',
+				'anagrafica.phone'                     => 'nullable|string|min:10|regex: /[0-9]/',
+				'anagrafica.fax'                       => 'nullable|string|min:10|regex: /[0-9]/',
+				'anagrafica.email'                     => 'nullable|string|email|max:100|unique:anagrafiche,email,' . $this->anagrafica->id,
+				'anagrafica.email_pec'                 => 'nullable|string|email|max:100|unique:anagrafiche,email_pec,' . $this->anagrafica->id,
+				'anagrafica.ticket_code'               => 'nullable|string',
+				'anagrafica.date_of_birth'             => 'nullable|date|date_format:Y-m-d|before:today',
+				'anagrafica.common_of_birth'           => 'nullable|string',
+				'anagrafica.province_of_birth'         => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
+				'anagrafica.order_or_college'          => 'nullable|string',
+				'anagrafica.order_or_college_province' => 'nullable|string|min:2|max:2|regex: /[A-Za-z]/',
+				'anagrafica.order_or_college_number'   => 'nullable|string|regex: /[0-9]/',
 			];
 		}
 
@@ -66,41 +43,17 @@
 			return '7xl';
 		}
 
-		public function mount($id) {
-			$this->anagrafica = Anagrafica::find($id);
-			$this->subject_type = $this->anagrafica->subject_type;
-			$this->consultant_type = $this->anagrafica->consultant_type;
-			$this->company_name = $this->anagrafica->company_name;
-			$this->first_name = $this->anagrafica->first_name;
-			$this->last_name = $this->anagrafica->last_name;
-			$this->address = $this->anagrafica->address;
-			$this->zip = $this->anagrafica->zip;
-			$this->city = $this->anagrafica->city;
-			$this->province = $this->anagrafica->province;
-			$this->iban = $this->anagrafica->iban;
-			$this->vat = $this->anagrafica->vat;
-			$this->fiscal_code = $this->anagrafica->fiscal_code;
-			$this->phone = $this->anagrafica->phone;
-			$this->fax = $this->anagrafica->fax;
-			$this->email = $this->anagrafica->email;
-			$this->email_pec = $this->anagrafica->email_pec;
-			$this->ticket_code = $this->anagrafica->ticket_code;
-			$this->date_of_birth = $this->anagrafica->date_of_birth;
-			$this->common_of_birth = $this->anagrafica->common_of_birth;
-			$this->province_of_birth = $this->anagrafica->province_of_birth;
-			$this->order_or_college = $this->anagrafica->order_or_college;
-			$this->order_or_college_province = $this->anagrafica->order_or_college_province;
-			$this->order_or_college_number = $this->anagrafica->order_or_college_number;
+		public function mount(Anagrafica $anagrafica) {
+			$this->anagrafica = $anagrafica;
 			foreach ($this->anagrafica->roles->pluck('id') as $role) {
 				$this->roles[] = $role;
 			}
-
 			$this->subject_roles = SubjectRole::all();
 		}
 
 		public function save() {
-			$validated = $this->validate();
-			$this->anagrafica->update($validated);
+			$this->validate();
+			$this->anagrafica->update();
 			$this->anagrafica->roles()->sync($this->roles);
 			$this->closeModalWithEvents([
 				Index::getName() => 'anagrafica-updated',
