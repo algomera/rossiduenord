e31001147d97194b27fe6dedf4dbfba2797da4e7
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2022-05-25 10:02:02 +0200

    change password

--- rossiduenord/database/seeds/UserSeeder.php
+++ rossiduenord/database/seeds/UserSeeder.php
@@ -18,29 +18,29 @@ class UserSeeder extends Seeder
         // Utente Admin
         $admin = User::create([
             'email' => 'admin@example.test',
-            'password' => bcrypt('password'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
 
         // Utenti Impresa
         $primehub = User::create([
             'email' => 'info@primehub.it',
-            'password' => bcrypt('mtmopx9m'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
         $edrasis = User::create([
             'email' => 'info@edrasis.it',
-            'password' => bcrypt('mtmopx9m'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
         $novello = User::create([
             'email' => 'p.novello@integrabusiness.net',
-            'password' => bcrypt('mtmopx9m'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
         $tasrl = User::create([
             'email' => 'tasrl1202@gmail.com',
-            'password' => bcrypt('mtmopx9m'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
         $mulas = User::create([
             'email' => 'stefano.mulas@ibigroup.it',
-            'password' => bcrypt('stefano001'),
+            'password' => bcrypt('%5Bcontent'),
         ]);
 
         // Creo UserData per admin
