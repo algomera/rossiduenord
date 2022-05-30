a5e2fb34b7eed1ae4fc5fb2a5cb19b61ded9eb54
fabio-alkimia <fabio@alkimiadigital.it>
2022-05-30 10:29:28 +0200

    - Fix responsive componente "practice-info";

--- rossiduenord/resources/views/livewire/practice-info.blade.php
+++ rossiduenord/resources/views/livewire/practice-info.blade.php
@@ -1,20 +1,20 @@
-<div class="fixed bottom-0 left-[280px] right-0 border-t bg-white flex px-4 py-3">
+<div class="fixed bottom-0 left-0 right-0 lg:left-[280px] border-t bg-white flex px-4 py-3">
 	<div class="w-full flex justify-between items-center">
-		<div class="flex items-center space-x-2">
-			<span class="text-sm text-blue-400">N° Pratiche in lista</span>
+		<div class="flex flex-col items-center space-y-1 lg:space-y-0 lg:space-x-2 lg:flex-row">
+			<span class="text-[9px] lg:text-sm text-blue-400">N° Pratiche in lista</span>
 			<div class="border-2 border-blue-300 px-4 w-32 text-center">
 				<span class="text-sm font-bold">{{$practices->count()}}</span>
 			</div>
 		</div>
-		<div class="flex space-x-6 text-center">
-			<div class="flex items-center space-x-2">
-				<span class="text-sm text-blue-400">Importo SAL €</span>
+		<div class="flex space-x-2 text-center lg:space-x-6">
+			<div class="flex flex-col items-center space-y-1 lg:space-y-0 space-x-2 lg:flex-row">
+				<span class="text-[9px] lg:text-sm text-blue-400">Importo SAL €</span>
 				<div class="border-2 border-blue-300 px-4 w-32 text-center">
 					<span class="text-sm font-bold">{{ Money::format($practices->sum('import_sal')) }}</span>
 				</div>
 			</div>
-			<div class="flex items-center space-x-2">
-				<span class="text-sm text-blue-400">Importo SAL stimato €</span>
+			<div class="flex flex-col items-center space-y-1 lg:space-y-0 lg:space-x-2 lg:flex-row">
+				<span class="text-[9px] lg:text-sm text-blue-400">Importo SAL stimato €</span>
 				<div class="border-2 border-blue-300 px-4 w-32 text-center">
 					<span class="text-sm font-bold">{{ Money::format($practices->sum('import')) }}</span>
 				</div>
