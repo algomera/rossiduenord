c157cc4fea16632a8a66333f33b4532495a12010
fabio-alkimia <fabio@alkimiadigital.it>
2022-06-07 11:25:17 +0200

    - Aggiunta commenti esplicativi a users_businesses.php;

--- rossiduenord/config/users_businesses.php
+++ rossiduenord/config/users_businesses.php
@@ -1,8 +1,10 @@
 <?php
 	return [
+		// Lista dei ruoli a cui viene automaticamente associato un utente creato
 		'from' => [
 			'business',
 		],
+		// Lista dei ruoli selezionabili per far vedere la lista delle imprese a cui associarli
 		'to' => [
 			'technical_asseverator',
 			'fiscal_asseverator',
