c426f3ef708a65dc5020e9cc16105508edd7112e
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2022-06-15 15:34:41 +0200

    delete error

--- rossiduenord/app/Http/Controllers/Auth/LoginController.php
+++ rossiduenord/app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@
 		}
 
 		public function login(Request $request) {
-			$credential = $request->validate(['email' => 'required|email', 'password' => 'required',]);
+			$credentials = $request->validate(['email' => 'required|email', 'password' => 'required',]);
 			if (Auth::attempt($credentials, $request->has('remember'))) {
 				$request->session()->regenerate();
 				if (auth()->user()->role === 'business') {
