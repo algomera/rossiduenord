0b3b2ca98d837e159866a8566e1cd2be9080a66a
fabio-alkimia <fabio@alkimiadigital.it>
2022-06-23 09:29:37 +0200

    - Rimosse righe non più necessarie che facevano riferimento alla vecchia piattaforma dal file routes/web.php;

--- rossiduenord/routes/web.php
+++ rossiduenord/routes/web.php
@@ -61,48 +61,9 @@
 				Route::get('/price_list', [PriceListIndex::class, '__invoke'])->name('index');
 			});
 
-			/* Scheda Pratica */
-			//Richiedente
-//			Route::resource('/applicant', 'ApplicantController');
-			//Subappaltarori
-//			Route::resource('/subject', 'SubjectController');
-//			Route::get('/subject/{practice}', 'SubjectController@subject_edit')->where('practice', '[0-9]+')->name('subject_edit');
-//			Route::post('/subject/{practice}/set', 'SubjectController@setSubject');
-			//Immobile
-//			Route::resource('/building', 'BuildingController');
-			//Foto/Video
-//			Route::get('/medias/{practice}', 'MediaController@index')->name('medias');
-//			Route::get('photos_practice', function (Request $request) {
-//				$user = $request->user()->id;
-//				$practice = Practice::where('user_id', $user)->pluck('id');
-//				$photos = Photo::where('practice_id', $practice)->get();
-//				return PhotoResource::collection($photos);
-//			});
-//			Route::get('videos_practice', function (Request $request) {
-//				$user = $request->user()->id;
-//				$practice = Practice::where('user_id', $user)->pluck('id');
-//				$videos = Video::where('practice_id', $practice)->get();
-//				return VideoResource::collection($videos);
-//			});
-			//Documenti Richiesti
-//			Route::get('/folder_document/{practice}/{folder_document}', 'FolderDocumentController@show')->name('folderDocument.show');
-//			Route::get('/folder_document/{practice}/{folder_document}/{sub_folder}', 'FolderDocumentController@show_document')->name('document.show');
-//			Route::get('/folder_document/{practice}/{folder_document}/{sub_folder}/approve', 'FolderDocumentController@approve_sub_folder')->name('document.approve');
-//			Route::get('/folder_document/{practice}/{folder_document}/{sub_folder}/disapprove', 'FolderDocumentController@disapprove_sub_folder')->name('document.disapprove');
-//			Route::post('/folder_document/{practice}/{folder_document}/{sub_folder}/store', 'FolderDocumentController@store')->name('document.store');
-//			Route::get('/doument/download/{document}', 'FolderDocumentController@downloadDocument')->name('document.download');
-//			Route::delete('/folder_document/{practice}/{folder_document}/{sub_folder}/{document}/delete', 'FolderDocumentController@destroy')->name('document.destroy');
 			//Superbonus
-			Route::get('/superbonus/{practice}', 'SuperBonusController@index')->where('practice', '[0-9]+')->name('superbonus.index');
-			Route::get('/superbonus/{practice}/{building}', 'SuperBonusController@show')->where('practice', '[0-9]+')->where('building', '[0-9]+')->name('superbonus.show');
-			Route::put('/superbonus/data_project/{practice}/update', 'SuperBonusController@update_data_project')->where('practice', '[0-9]+')->name('update_data_project');
-			Route::get('/superbonus/driving_intervention/{practice}/{type?}', 'SuperBonusController@driving_intervention')->where('practice', '[0-9]+')->name('driving_intervention');
-			Route::put('/superbonus/driving_intervention/{practice}/update', 'SuperBonusController@update_driving_intervention')->where('practice', '[0-9]+')->name('update_driving_intervention');
-			Route::get('/superbonus/towed_intervention/{practice}/{condomino?}/{type?}', 'SuperBonusController@towed_intervention')->where('practice', '[0-9]+')->name('towed_intervention');
-			Route::put('/superbonus/towed_intervention/{practice}/update', 'SuperBonusController@update_towed_intervention')->where('practice', '[0-9]+')->name('update_towed_vertical_wall');
 			Route::get('/superbonus/final_state/{practice}', 'SuperBonusController@final_state')->where('practice', '[0-9]+')->name('final_state');
 			Route::put('/superbonus/final_state/{practice}/update', 'SuperBonusController@update_final_state')->where('practice', '[0-9]+')->name('update_final_state');
-			Route::get('/superbonus/final_state/{practice}', 'SuperBonusController@final_state')->where('practice', '[0-9]+')->name('final_state');
 			Route::get('/superbonus/fees_declaration/{practice}', 'SuperBonusController@fees_declaration')->where('practice', '[0-9]+')->name('fees_declaration');
 			Route::get('/superbonus/var_computation/{practice}', 'SuperBonusController@var_computation')->where('practice', '[0-9]+')->name('var_computation');
 			Route::put('/superbonus/var_computation/{practice}/update', 'SuperBonusController@update_var_computation')->where('practice', '[0-9]+')->name('update_var_computation');
@@ -121,31 +82,6 @@
 					}
 				}
 			});
-			Route::delete('/surface/{id}/delete', 'SuperBonusController@delete_surface');
-			//Condomini
-			Route::post('/save_condomino_data/{id}', function ($id, Request $request) {
-				$pid = $request->get('practice');
-				$practice = Practice::find($pid);
-				// Update Condomino Datas
-				if ($id !== "common") {
-					$data = json_decode($request->get('data'), true);
-					$condomino = Condomini::find($id);
-					$condomino->update($data);
-				}
-				// Update Interventi
-				Interventi::addCondensingBoiler($practice, $request->get('interventi')['condensing_boilers']);
-				Interventi::addHeatPump($practice, $request->get('interventi')['heat_pumps']);
-				Interventi::addAbsorptionHeatPump($practice, $request->get('interventi')['absorption_heat_pumps']);
-				Interventi::addHybridSystem($practice, $request->get('interventi')['hybrid_systems']);
-				Interventi::addMicrogenerationSystem($practice, $request->get('interventi')['microgeneration_systems']);
-				Interventi::addWaterHeatpumpsInstallation($practice, $request->get('interventi')['water_heatpumps_installations']);
-				Interventi::addBiomeGenerator($practice, $request->get('interventi')['biome_generators']);
-				Interventi::addSolarPanel($practice, $request->get('interventi')['solar_panels']);
-				return "updated";
-			});
-			//Excel condomini
-//			Route::get('/download/excel/{practiceId}', 'BuildingController@downloadExcel')->name('downloadExcel');
-//			Route::delete('/delete/excel/{practiceId}', 'BuildingController@deleteExcel')->name('deleteExcel');
 			// Intervention delete
 			Route::delete('/condensing_boilers/{id}/delete', 'InterventionController@deleteCondensingBoilers');
 			Route::delete('/heat_pumps/{id}/delete', 'InterventionController@deleteHeatPumps');
@@ -155,23 +91,5 @@
 			Route::delete('/water_heatpumps_installations/{id}/delete', 'InterventionController@deleteWaterHeatpumpsInstallations');
 			Route::delete('/biome_generators/{id}/delete', 'InterventionController@deleteBiomeGenerators');
 			Route::delete('/solar_panels/{id}/delete', 'InterventionController@deleteSolarPanels');
-			// condomini export
-//			Route::get('/condomini_export/{practice}', 'CondominiController@export')->name('condomini.export');
-			//contract
-			Route::get('/contracts/{practice}', 'ContractController@originalIndex')->name('contracts.index');
-			Route::get('/contracts/signed/{contract}', 'ContractController@signedIndex')->name('signed.index');
-			Route::post('/new/signed/{contract}', 'ContractController@signedStore')->name('signed.store');
-			Route::get('/signed/{signed}', 'ContractController@signedShow')->name('signed.show');
-			Route::get('/contract/download/{id}', 'ContractController@contractDownload')->name('contract.download');
-			Route::get('/contract/signed/download/{id}', 'ContractController@signedDownload')->name('signed.download');
-			Route::delete('/signed/delete/{signed}', 'ContractController@deleteSigned')->name('contracts.signed.delete');
-			//policies
-			Route::get('/policies/{practice}', 'PolicyController@index')->name('policies.index');
-			Route::get('/policy/download/{id}', 'PolicyController@policyDownload')->name('policy.download');
-			Route::get('/policies/signed/{policy}', 'PolicyController@signedIndex')->name('policies.signed');
-			Route::post('/new/signedpolicy/{policy}', 'PolicyController@signedStore')->name('policies.signed.store');
-			Route::get('/policies/modified/{id}/download', 'PolicyController@modifiedDownload')->name('modified.download');
-			Route::get('/policies/modified/{modified}', 'PolicyController@modifiedShow')->name('modified.show');
-			Route::delete('/modified/delete/{modified}', 'PolicyController@deleteModified')->name('policies.modified.delete');
 		});
 	});
